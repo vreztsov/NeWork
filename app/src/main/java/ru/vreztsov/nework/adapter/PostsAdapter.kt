ff6b6cf84717a126d0627a95c73219e00aa6c11b
vreztsov <31355847+vreztsov@users.noreply.github.com>
2024-12-12 20:16:36 +0300

    Add post attachments

--- app/src/main/java/ru/vreztsov/nework/adapter/PostsAdapter.kt
+++ app/src/main/java/ru/vreztsov/nework/adapter/PostsAdapter.kt
@@ -4,6 +4,8 @@ import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.VideoView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
@@ -13,9 +15,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import ru.vreztsov.nework.R
 import ru.vreztsov.nework.databinding.CardPostBinding
+import ru.vreztsov.nework.dto.AttachmentType
 import ru.vreztsov.nework.dto.Post
 import ru.vreztsov.nework.util.DataViewTransform
 
@@ -25,6 +29,8 @@ interface PostsOnInteractionListener {
     fun onRemove(post: Post)
     fun onShare(post: Post)
     fun onPostCLick(post: Post)
+    fun onPlayVideo(post: Post, videoView: VideoView, button: ImageButton)
+    fun onPlayAudio(post: Post, button: ImageButton)
     // fun onAttachClick()
 
 }
@@ -116,7 +122,29 @@ class PostViewHolder(
                 .into(avatar)
             menu.isVisible = post.ownedByMe
             //TODO attachment
-            attachmentContent.visibility = View.GONE
+            post.attachment?.run {
+                attachmentContent.visibility = View.VISIBLE
+                if (type == AttachmentType.IMAGE) {
+                    Glide.with(root.context)
+                        .load(url)
+                        .error(R.drawable.ic_broken_image_24)
+                        .apply(
+                            RequestOptions.overrideOf(
+                                root.resources.displayMetrics.widthPixels
+                            )
+                        )
+                        .timeout(2_000)
+                        .into(imageAttachment)
+                }
+                imageAttachment.isVisible = (type == AttachmentType.IMAGE)
+                videoAttachment.isVisible = (type == AttachmentType.VIDEO)
+                playButtonPost.isVisible = (type == AttachmentType.VIDEO)
+                playButtonPostAudio.isVisible = (type == AttachmentType.AUDIO)
+                descriptionAttachment.isVisible = (type == AttachmentType.AUDIO)
+            } ?: {
+                attachmentContent.visibility = View.GONE
+            }
+
             postListeners(post)
         }
     }
@@ -129,6 +157,12 @@ class PostViewHolder(
             }
             share.setOnClickListener {
                 onInteractionListener.onShare(post)
+            }
+            playButtonPost.setOnClickListener {
+                onInteractionListener.onPlayVideo(post, binding.videoAttachment, binding.playButtonPost)
+            }
+            playButtonPostAudio.setOnClickListener {
+                onInteractionListener.onPlayAudio(post, binding.playButtonPostAudio)
             }
             //TODO attachment
         }
--- app/src/main/java/ru/vreztsov/nework/ui/PostsFragment.kt
+++ app/src/main/java/ru/vreztsov/nework/ui/PostsFragment.kt
@@ -1,10 +1,16 @@
 package ru.vreztsov.nework.ui
 
 import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -12,6 +18,7 @@ import ru.vreztsov.nework.R
 import ru.vreztsov.nework.adapter.PostsAdapter
 import ru.vreztsov.nework.adapter.PostsOnInteractionListener
 import ru.vreztsov.nework.databinding.FragmentPostsBinding
+import ru.vreztsov.nework.dto.AttachmentType
 import ru.vreztsov.nework.dto.Post
 import ru.vreztsov.nework.util.ARG_POST_ID
 import ru.vreztsov.nework.util.goToLogin
@@ -25,6 +32,10 @@ class PostsFragment : Fragment() {
     private lateinit var binding: FragmentPostsBinding
     private lateinit var adapter: PostsAdapter
     private val viewModel: PostViewModel by activityViewModels()
+    private val mediaPlayer = MediaPlayer()
+    //TODO приделай меню для правки своих постов
+    //TODO не забудь про создание нового поста
+//    TODO про обновление страницы и пагинацию тоже подумай
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -77,6 +88,69 @@ class PostsFragment : Fragment() {
                         putLong(ARG_POST_ID, post.id)
                     }
                 )
+            }
+
+            override fun onPlayVideo(post: Post, videoView: VideoView, button: ImageButton) {
+                if (post.attachment?.type == AttachmentType.VIDEO) {
+                    videoView.apply {
+                        isVisible = true
+                        setVideoURI(Uri.parse(post.attachment.url))
+                        setMediaController(MediaController(requireContext()))
+                        setOnPreparedListener {
+                            layoutParams?.height =
+                                (resources.displayMetrics.widthPixels *
+                                        (it.videoHeight.toDouble() / it.videoWidth)).toInt()
+                            start()
+                            button.visibility = View.GONE
+//                            if (!isPlaying) {
+//                                layoutParams?.height =
+//                                    (resources.displayMetrics.widthPixels *
+//                                            (it.videoHeight.toDouble() / it.videoWidth)).toInt()
+//                                start()
+//                                CoroutineScope(EmptyCoroutineContext).launch {
+//                                    playButton.isChecked = true
+//                                    delay(3_000)
+//                                    playButton.visibility = View.INVISIBLE
+//                                }
+//                            } else {
+//                                pause()
+//                                playButton.isChecked = false
+//                            }
+                        }
+//                        setOnClickListener {
+//                            if (isPlaying) CoroutineScope(EmptyCoroutineContext).launch {
+//                                playButton.visibility = View.VISIBLE
+//                                delay(3_000)
+//                                playButton.visibility = View.INVISIBLE
+//                            }
+//                        }
+                        setOnCompletionListener {
+                            layoutParams?.let {
+                                it.width = resources.displayMetrics.widthPixels
+                                it.height = (it.width * 0.5625).toInt()
+                            }
+                            stopPlayback()
+                            button.visibility = View.VISIBLE
+                        }
+                    }
+                }
+            }
+
+            override fun onPlayAudio(post: Post, button: ImageButton) {
+                post.attachment
+                    ?.takeIf { it.type == AttachmentType.AUDIO }
+                    ?.let {
+                        if (mediaPlayer.isPlaying) {
+                            mediaPlayer.pause()
+                            button.setImageResource(R.drawable.ic_play_circle)
+                        } else {
+                            mediaPlayer.reset()
+                            mediaPlayer.setDataSource(it.url)
+                            mediaPlayer.prepare()
+                            mediaPlayer.start()
+                            button.setImageResource(R.drawable.ic_pause_circle)
+                        }
+                    }
             }
         }
     )
